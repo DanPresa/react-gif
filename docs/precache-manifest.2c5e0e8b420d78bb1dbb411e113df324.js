d2c92fd7ff646d41fa3fab8001b9a66de4e58c56
Daniel <danielpresac@gmail.com>
2020-07-09 16:40:08 -0500

    Renombrar build to docs

---
+++ docs/precache-manifest.2c5e0e8b420d78bb1dbb411e113df324.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6995e5490c60dc70dab4c97b2c95e99a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3ddd396fd8cbfc12b24",
+    "url": "/static/css/main.b199c3cb.chunk.css"
+  },
+  {
+    "revision": "96be619ca1ecd9993a62",
+    "url": "/static/js/2.f05fd237.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f05fd237.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3ddd396fd8cbfc12b24",
+    "url": "/static/js/main.1d1c5531.chunk.js"
+  },
+  {
+    "revision": "39e88660e5d512526393",
+    "url": "/static/js/runtime-main.3c337214.js"
+  }
+]);
